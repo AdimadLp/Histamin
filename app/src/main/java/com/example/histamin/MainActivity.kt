d3ce4324eb74affe32b92906eeb72dec108dd527
AdimadLp <AdimadLp@users.noreply.github.com>
2021-09-07 02:51:07 +0200

    Searchbar bugfix Searchbar filter setup

--- app/src/main/java/com/example/histamin/MainActivity.kt
+++ app/src/main/java/com/example/histamin/MainActivity.kt
@@ -1,43 +1,36 @@
 package com.example.histamin
 
 import android.os.Bundle
-import android.view.Menu
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.SearchView
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.histamin.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
 
     private lateinit var adapter: ArrayAdapter<*>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        val lvListView = findViewById<ListView>(R.id.lv_ListView)
+        val tvEmptyTextView = findViewById<TextView>(R.id.tv_EmptyTextView)
+        val searchbar = findViewById<SearchView>(R.id.nav_search)
+
         adapter = ArrayAdapter(
             this,
             android.R.layout.simple_list_item_1,
             resources.getStringArray(R.array.countries_array)
 
         )
-        binding.lvListView.adapter = adapter
-        binding.lvListView.emptyView = binding.tvEmptyTextView
-    }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.nav_menu, menu)
+        lvListView.adapter = adapter
+        lvListView.emptyView = tvEmptyTextView
 
-        val search = menu?.findItem(R.id.nav_search)
-        val searchView = search?.actionView as SearchView
-        searchView.queryHint = "Search something"
-
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+        searchbar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 return false
             }
@@ -46,8 +39,12 @@ class MainActivity : AppCompatActivity() {
                 adapter.filter.filter(newText)
                 return false
             }
-        })
-        return super.onCreateOptionsMenu(menu)
+        }
+        )
+
 
     }
 }
+
+
+
